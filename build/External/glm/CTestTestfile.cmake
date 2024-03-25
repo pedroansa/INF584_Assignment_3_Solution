a92d43166cec6ce8a6e0fc17bec5249bde995d6d
Pedro Andrade <pedroandrade_jp@hotmail.com>
2024-03-25 15:03:18 +0100

    Add build

---
+++ build/External/glm/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pedro/OneDrive/Área de Trabalho/INF584_Assignment_3_Solution/External/glm
+# Build directory: C:/Users/pedro/OneDrive/Área de Trabalho/INF584_Assignment_3_Solution/build/External/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
---
+++ build/External/glm/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pedro/OneDrive/Área de Trabalho/INF584_Assignment_3_Solution/External/glm/test
+# Build directory: C:/Users/pedro/OneDrive/Área de Trabalho/INF584_Assignment_3_Solution/build/External/glm/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
